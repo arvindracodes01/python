bc6c4d34eb39d27d5c1e725cf0adf1d02ef07b7b
Arvindra Yadav <thearvindra.nwg@gmail.com>
2026-08-12 17:40:36 +0530

    module example added

---
+++ module.py
@@ -0,0 +1,9 @@
+import calculater
+
+a = 20
+b = 5
+
+print("Addition:", calculater.add(a, b))
+print("Subtraction:", calculater.subtract(a, b))
+print("Multiplication:", calculater.multiply(a, b))
+print("Division:", calculater.divide(a, b))
